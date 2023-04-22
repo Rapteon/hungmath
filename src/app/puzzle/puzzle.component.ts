85f4ba8380aafcf52a381c04460bcd9895f9a844
Rapteon <rapteon@tutanota.com>
2023-04-22 17:42:52 +0530

    Added basic puzzle feature. No hangman enabled for now.

--- src/app/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Puzzle } from '../types/Puzzle';
 
 @Component({
   selector: 'puzzle',
   templateUrl: './puzzle.component.html',
-  styleUrls: ['./puzzle.component.css']
+  styleUrls: ['./puzzle.component.css'],
 })
-export class PuzzleComponent {
+export class PuzzleComponent implements OnInit {
+  puzzle: Puzzle;
 
+  @ViewChild('solution')
+  private _solutionField!: ElementRef<HTMLInputElement>;
+
+  constructor() {
+    this.puzzle = new Puzzle(10);
+  }
+
+  ngOnInit(): void {}
+
+  
+  checkSolution(solution: string) {
+    if (this.puzzle.check(solution)) {
+      alert('Correct');
+      this.puzzle = new Puzzle(10);
+      this._solutionField.nativeElement.value = '';
+      this._solutionField.nativeElement.focus();
+    }
+  }
 }
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent {
-
 }
